d3df8f8c37e02edf471f1fd29def2dd9b3cf137f
DINHHUAN <huanden246@gmail.com>
2025-09-19 22:36:01 +0700

    init project

---
+++ cart-service/src/test/java/com/dinhhuan/cart/CartServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dinhhuan.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
